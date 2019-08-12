a97cbc8caad4e7b6af77ec63737b112250cd3baf
Scott Davis <scott@thirstyhead.com>
2019-08-12 14:50:35 -0600

    first pass at toggle web component

---
+++ components/toggle/index.js
@@ -0,0 +1,37 @@
+'use strict';
+// For more on custom elements, see
+// https://developers.google.com/web/fundamentals/web-components/shadowdom
+
+window.customElements.define('th-toggle',
+  class ThToggle extends HTMLElement{
+    constructor(){
+      super();
+    }
+
+    connectedCallback(){
+      let shadowRoot = this.attachShadow({mode: 'open'});
+      shadowRoot.appendChild(this.styleBlock);
+      shadowRoot.appendChild(this.contentBlock);
+    }
+
+    get styleBlock(){
+      let styleBlock = document.createElement('style');
+      styleBlock.innerHTML = `
+        summary h2{
+          display: inline-block;
+        }
+      `;
+      return styleBlock;
+    }
+
+    get contentBlock(){
+      let contentBlock = document.createElement('details');
+      contentBlock.innerHTML = `
+        <summary>
+          <h2><slot name="summary"></slot></h2>
+        </summary>
+        <slot></slot>
+      `;
+      return contentBlock;
+    }
+  });
